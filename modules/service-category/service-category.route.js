0de6421163504a68e4eab1a08729c4e3caa9dfd6
imohit1o1 <mohitk1595@gmail.com>
2026-03-08 00:43:47 +0530

    feat: remove authentication requirement for all service category routes.

--- modules/service-category/service-category.route.js
+++ modules/service-category/service-category.route.js
@@ -1,14 +1,11 @@
 import { Router } from "express"
-import { authenticate, validate } from "../../shared/middlewares/index.middleware.js"
+import { validate } from "../../shared/middlewares/index.middleware.js"
 import { CategoryController } from "./service-category.controller.js"
 import { CategoryValidator } from "./service-category.validator.js"
 
 const router = Router()
 
-// All category routes require authentication
-router.use(authenticate)
-
-// All authenticated roles can view categories
+// All category routes
 router.get("/", CategoryController.getAllCategories)
 router.get("/slug/:slug", CategoryController.getCategoryBySlug)
 router.get("/:id", validate(CategoryValidator.categoryIdSchema, "params"), CategoryController.getCategoryById)
